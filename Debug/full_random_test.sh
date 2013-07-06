3917198841d029b555c676cf0c9ae7dfcee4ed5a
Merry38 <rose.tia38@gmail.com>
2013-07-06 16:42:35 +0200

    Added time difference measument between executions

--- Debug/full_random_test.sh
+++ Debug/full_random_test.sh
@@ -18,12 +18,19 @@ do
 
 	echo ".::Solving $FILENAME::.";
 
+	OPT_START=$(date +%s%N);
 	$EXE $1 $i > $OPT_FILENAME 2>&1;
+	OPT_STOP=$(date +%s%N);
+
+	PREOPT_START=$(date +%s%N);
 	$PREOPT_EXE $1 $i > $PREOPT_FILENAME 2>&1;
+	PREOPT_STOP=$(date +%s%N);
 
 	SYNTHESIS_FILENAME=$SYNTHESIS"synthesis_result"$NOW;
 
-	echo -e ".::Instance $FILENAME::.\nDiffering lines:" >> $SYNTHESIS_FILENAME;
+	TIME_DIFFERENCE=$(($(($OPT_STOP-$OPT_START))-$(($PREOPT_STOP-$PREOPT_START))));
+
+	echo -e ".::Instance $FILENAME::.\nTime difference: $TIME_DIFFERENCE ns\nDiffering lines:" >> $SYNTHESIS_FILENAME;
 	diff $OPT_FILENAME $PREOPT_FILENAME >> $SYNTHESIS_FILENAME;
 	echo "" >> $SYNTHESIS_FILENAME;
 done;
